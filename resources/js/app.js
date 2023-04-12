6fb9f836d79b807f291c5b0e79665f9eb4f07894
safipeti <safipeti@gmail.com>
2023-04-12 15:08:10 +0200

    Installálás: vue 3 és alapkomponens létrehozása

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import { createApp } from "vue";
+import App from "./vue/components/App";
+
+const app = createApp({});
+
+app.component("app", App);
+
+app.mount("#app");
---
+++ resources/js/vue/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Title</div>
+
+                    <div class="card-body">Todo App - Task manager</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
